73d9cbcdd7254b7dcdec0eb204e69e5217448277
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-06-24 13:30:12 +0530

    add Loop and Array

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let firstCard = 11;
 let secondCard = 7;
-let cards =[firstCard,secondCard]
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true;
@@ -9,13 +9,16 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el")
 
-function startGame(){
+function startGame() {
     renderGame()
 }
 
 function renderGame() {
     sumEl.textContent = "Sum: " + sum;
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    cardsEl.textContent = "Cards: ";
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
